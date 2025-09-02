c2115022a850a8ec08593414ec49f55244a33548
gaellebega <gaellebega@gmail.com>
2025-09-03 00:27:37 +0200

    mehtods used  on numbers

--- methods.js
+++ methods.js
@@ -1,5 +1,6 @@
 //THIS IS SAMPLE OF HOW TO USE THE METHODS
 
+//the String methods
 let greetings="  Hello World  ";
 //by using the length we dontt add the 2 brackets
 console.log(greetings.length);
@@ -18,12 +19,15 @@ console.log(greetings.includes("Hello"));
 console.log(greetings.indexOf("l"));
 console.log(greetings.lastIndexOf("H"));
 console.log(greetings.lastIndexOf("l"));
+console.log(greetings.replace("Hello","hyyyy"));
 //to use the slice you write the index you wanna start froma and the end before the number been said.
 //to use the slice without 2 numbers that means that you start from that first then the others follow in the normal 
 console.log(greetings.slice(0,2));
 //the split is used to split the strings inside the array it return an array nt a stirng
 //to mean is like telling the computer that evrythime you reach to that character bring the string that you have before it
+//count the space and make them an empty string when are 2 when they are 1 it ignores it
 console.log(greetings.split(" "));
+
 let name="Gagaayle"
 let result=name.split("a");
 console.log(result);
@@ -39,6 +43,9 @@ console.log(names.split("e"));
  console.log(country.slice(3));
  console.log(country.slice(-3));
 
+
+ //this is how how the question was asking to do this after store our data in value
+
  let str=" Programming is fun! ";
  let trimme=str.trim()
  console.log(trimme);
@@ -46,3 +53,34 @@ console.log(names.split("e"));
  console.log(trimme.toLowerCase());
  console.log(trimme.length);
  console.log(trimme.slice(0,12));
+
+ //The numbers methods
+
+ let nc=3.5;
+ console.log(nc.toFixed(2));
+ console.log(nc.toString());
+ //this is boolean we have to write Number before
+ console.log(Number.isInteger(nc));
+ //on the following below we dont need to add the calling of our variable
+ console.log(parseInt("3.5"));
+ console.log(parseFloat(3))
+
+//Numbers methods
+
+const number=123.45;
+console.log(number.toFixed());
+
+let result2=number.toString();
+console.log(result2);
+console.log(number.toString());
+
+console.log(Number.isInteger());
+// const number2="123.45";
+//the below examples all take the strings
+//the parseInt and the parseFloat they are not the functions
+console.log(parseInt("123.5abc"));
+
+console.log(parseFloat("12.45"));
+
+
+//THE ARRAYS METHODS
--- test.js
+++ test.js
@@ -25,7 +25,7 @@ console.log(` Hello Beautiful ${name}`);
 }
 sayName("Gaelle");
 sayName("Lorraine");
-sayName("Galila");
+sayName("Galirene"); 
 
 //another role of the string intrepolation
 const paragraph=`hello guys i just wante do
@@ -35,5 +35,25 @@ because at first day i was able to write only 25 wpm
 then i made a goal of writting 60 wpm by the end of August
 and now its 59wpm on the 28th of August,
 my advice to evryone is that considtensy is the key to success
-if you want to achieve something you have to work hard for it`;
+if you want to achieve something you hav
+e to work hard for it`;
 console.log(paragraph);
+
+const name2="this is my name";
+//when you dont open the quotes it will consider the whole thing as one string
+console.log(name2.split(""));
+//when the string is like 3
+// console.log(name2.split("   "));
+const name3=" this is my name ";
+console.log(name3.split(""));
+
+const name4="  this   is   my   name  ";
+console.log(name4.split(""));
+
+const name5="this,is,my,name";
+console.log(name5.split(" "));
+
+const name6="this is my name";
+console.log(name6.split(" "));
+const names="my names are uwimana anageliqur"
+console.log(names.replace("anageliqur","ange"));
